38b5d71f26346a78a4253eef875b5f153fc4e10a
exipilis <malenko.andrii@gmail.com>
2018-09-06 15:17:29 +0300

    initial commit

---
+++ read.py
@@ -0,0 +1,9 @@
+import cv2
+
+from utils import detect_board_opencv, detect_board_color
+
+img = cv2.imread('s.png')
+
+# board = detect_board_opencv(img)
+board = detect_board_color(img)
+print(board)
